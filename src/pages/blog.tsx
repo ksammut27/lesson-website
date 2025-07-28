e51edf34dfc97b1adfe8736c42ff429becfe1024
Glenn Wallace <34867563+glennw-mt@users.noreply.github.com>
2025-07-28 02:15:57 +0200

    Revert "Update blog.tsx"

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -70,7 +70,6 @@ const Blog = ({ data }) => {
                   link={"/blog_post/"+post.slug}
                   title={post.title}
                   date={post.date}
-                  bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
                 />)
               })}
             </div>
